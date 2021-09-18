528ded5018ecef843b5133f1ffad5b2fad736549
yxj0312 <yanxiajun@hotmail.com>
2021-09-18 23:21:06 +0200

    install npm package and add baseInput to view

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -15,30 +15,24 @@
 
       <h3>Name & describe your event</h3>
 
-      <label>Title</label>
-      <input
+      <BaseInput
         v-model="event.title"
+        label="Title"
         type="text"
-        placeholder="Title"
-        class="field"
-      >
+      />
 
-      <label>Description</label>
-      <input
-        v-model="event.description"
+      <BaseInput
+        v-model="event.descrption"
+        label="Description"
         type="text"
-        placeholder="Description"
-        class="field"
       />
 
       <h3>Where is your event?</h3>
 
-      <label>Location</label>
-      <input
+      <BaseInput
         v-model="event.location"
+        label="Location"
         type="text"
-        placeholder="Location"
-        class="field"
       />
 
       <h3>Are pets allowed?</h3>
