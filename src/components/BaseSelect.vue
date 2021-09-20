d50025132fbb79333e75459953d2ab052352044c
yxj0312 <yanxiajun@hotmail.com>
2021-09-20 21:27:25 +0200

    add baseInput

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,22 @@
+<template>
+    <label v-if="label">{{ label }}</label>
+      <select v-model="event.category">
+        <option
+          v-for="option in categories"
+          :value="option"
+          :key="option"
+          :selected="option === event.category"
+        >{{ option }}</option>
+      </select>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
