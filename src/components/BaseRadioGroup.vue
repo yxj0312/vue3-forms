2311a870450f93e7188eb5ff7205cc300dc11f75
yxj0312 <yanxiajun@hotmail.com>
2021-09-24 22:41:35 +0200

    add BaseRadio to Group

--- src/components/BaseRadioGroup.vue
+++ src/components/BaseRadioGroup.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-
-    </div>
+    <BaseRadio
+        v-for="option in options"
+    />
 </template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
