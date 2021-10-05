6f704cc4fa104dc79314b4d9967018606b445b13
yxj0312 <yanxiajun@hotmail.com>
2021-10-05 15:57:42 +0200

    refactor select

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,36 +1,66 @@
 <template>
-    <label v-if="label">{{ label }}</label>
-      <select
-        :value="modelValue"
-        class="field"
-        v-bind="{
-            ...$attrs,
-            onChange: ($event) => { $emit('update:modelValue', $event.target.value)}
-        }"
-      >
-        <option
-          v-for="option in options"
-          :value="option"
-          :key="option"
-          :selected="option === modelValue"
-        >{{ option }}</option>
-      </select>
+  <label
+    v-if="label"
+    :for="uuid"
+  >
+    {{ label }}
+  </label>
+  <select
+    class="field"
+    v-bind="{
+      ...$attrs,
+      onChange: updateValue
+    }"
+    :value="modelValue"
+    :id="uuid"
+    :aria-describedby="error ? `${uuid}-error` : null"
+    :aria-invalid="error ? true : false"
+    :class="{ error }"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >
+      {{ option }}
+    </option>
+  </select>
+  <BaseErrorMessage
+    v-if="error"
+    :id="`${uuid}-error`"
+  >
+    {{ error }}
+  </BaseErrorMessage>
 </template>
 
 <script>
+import SetupFormComponent from '@/features/SetupFormComponent'
+import UniqueID from '@/features/UniqueID'
 export default {
   props: {
+    options: {
+      type: Array,
+      required: true
+    },
     label: {
       type: String,
       default: ''
     },
-    modelValue: {
-      type: [String, Number],
+    error: {
+      type: String,
       default: ''
     },
-    options: {
-      type: Array,
-      required: true
+    modelValue: {
+      type: [String, Number]
+    }
+  },
+  setup (props, context) {
+    const { updateValue } = SetupFormComponent(props, context)
+    const uuid = UniqueID().getID()
+    return {
+      updateValue,
+      uuid
     }
   }
 }
