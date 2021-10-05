35cfa5e2e594f5ae3e48e4c9ba7a8c33549e8eaa
yxj0312 <yanxiajun@hotmail.com>
2021-10-05 15:53:17 +0200

    refactor checkbox

--- src/components/BaseCheckbox.vue
+++ src/components/BaseCheckbox.vue
@@ -1,15 +1,28 @@
 <template>
-    <input
-        type="checkbox"
-        :checked="modelValue"
-        @change="$emit('update:modelValue', $event.target.checked)"
-        class="field"
-        v-bind="$attrs"
-    />
-    <label v-if="label">{{ label }}</label>
+  <input
+    v-bind="{ ...$attrs, onChange: updateValue }"
+    :checked="modelValue"
+    :id="uuid"
+    type="checkbox"
+    class="field"
+  />
+  <label
+    :for="uuid"
+    v-if="label"
+  >
+    {{ label }}
+  </label>
+  <BaseErrorMessage
+    v-if="error"
+    :id="`${uuid}-error`"
+  >
+    {{ error }}
+  </BaseErrorMessage>
 </template>
 
 <script>
+import UniqueID from '@/features/UniqueID'
+import SetupFormComponent from '@/features/SetupFormComponent'
 export default {
   props: {
     label: {
@@ -17,8 +30,19 @@ export default {
       default: ''
     },
     modelValue: {
-      type: Boolean,
-      default: false
+      type: Boolean
+    },
+    error: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props, context) {
+    const uuid = UniqueID().getID()
+    const { updateValue } = SetupFormComponent(props, context)
+    return {
+      updateValue,
+      uuid
     }
   }
 }
